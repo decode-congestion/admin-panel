8c61179629d6c9ed4ca45d688af19e41c4f0e499
Nima Boscarino <nima.boscarino@gmail.com>
2019-11-10 00:30:38 -0800

    INIT! - react router, ant design, leaflet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Button } from 'antd'
+
+const AdminMap = () => {
+  const [lat, setLat] = useState(51.505)
+  const [lng, setLng] = useState(-0.09)
+  const [zoom, setZoom] = useState(13)
+
+  const position = [lat, lng]
+  return (
+    <Map style={{
+      height: '100vh',
+      width: '80vw'
+    }} center={position} zoom={zoom}>
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </Map>
+  )
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'row'
+      }}>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/buses"></Redirect>
+          </Route>
+          <Route exact path="/buses">
+            <Button>Hello world</Button>
+            <AdminMap />
+          </Route>
+          <Route exact path="/buses/:id">
+            <h1>Specific bus</h1>
+          </Route>
+          <Route exact path="/tasks">
+            <h1>Tasks</h1>
+          </Route>
+          <Route exact path="/stops">
+            <h1>All stops</h1>
+          </Route>
+          <Route exact path="/stops/:id">
+            <h1>Single stop</h1>
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Leaflet from 'leaflet'
+import './App.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
